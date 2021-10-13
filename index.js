51a47fed6be036d4f419a93ad1099229f076f275
jMisch44 <jennimisch@gmail.com>
2021-10-13 15:42:58 -0400

    exercise 4 completed

--- index.js
+++ index.js
@@ -38,9 +38,15 @@ function trimPropertiesMutation(obj) {
  * findLargestInteger([{ integer: 1 }, { integer: 3 }, { integer: 2 }]) // returns 3
  */
 function findLargestInteger(integers) {
-  // ✨ implement
+  let result = integers[0].integer
+  for(let i = 0; i < integers.length; i++) {
+    if(integers[i].integer > result) {
+      result = integers[i].integer
+    }
+  }
+  return result
 }
-
+console.log(findLargestInteger([{ integer: 1 }, { integer: 3 }, { integer: 2 }]))
 class Counter {
   /**
    * [Exercise 4A] Counter creates a counter
--- index.test.js
+++ index.test.js
@@ -22,11 +22,20 @@ describe('[Exercise 2] trimPropertiesMutation', () => {
     const actual = utils.trimPropertiesMutation(input)
     expect(actual).toEqual(expected)
   })
-  // test('[4] the object returned is the exact same one we passed in', () => {})
+  test('[4] the object returned is the exact same one we passed in', () => {
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const result = utils.trimPropertiesMutation(input)
+    expect(input).toEqual(result)
+  })
 })
 
 describe('[Exercise 3] findLargestInteger', () => {
-  // test('[5] returns the largest number in an array of objects { integer: 2 }', () => {})
+  test('[5] returns the largest number in an array of objects { integer: 2 }', () => {
+    const input = [{ integer: 1 }, { integer: 3 }, { integer: 2 }]
+    const expected = 3
+    const actual = utils.findLargestInteger(input)
+    expect(actual).toBe(expected)
+  })
 })
 
 describe('[Exercise 4] Counter', () => {
